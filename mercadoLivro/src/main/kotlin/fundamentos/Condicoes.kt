c8f96240f809b8af8c8f699e0f885cfebde5996f
dionis moreira <dionismoreira@MacBook-Pro-de-dionis.local>
2023-04-19 13:23:44 -0300

    Trabalhando com condicoes

--- mercadoLivro/src/main/kotlin/fundamentos/Condicoes.kt
+++ mercadoLivro/src/main/kotlin/fundamentos/Condicoes.kt
@@ -2,6 +2,10 @@ package fundamentos
 
 fun main(){
     parOuImpar(2)
+    resultadoNota(7)
+    resultadoNota(6)
+    resultadoNota(3)
+
 }
 
 fun parOuImpar(numero: Int) {
@@ -9,5 +13,20 @@ fun parOuImpar(numero: Int) {
         println("par")
     }else{
         println("impar")
+    }
+}
+/*
+regra: aprovado > 7
+       Recuperação > 5
+       Reprovado <= 4
+*/
+
+fun resultadoNota(nota: Int){
+    if (nota >= 7){
+        println("Aprovado")
+    }else if (nota >= 6){
+        println("Recuperação")
+    }else{
+        println("reprovado")
     }
 }
